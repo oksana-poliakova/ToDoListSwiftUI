e58ca17c04915f61f966ede50e32c78cb41a67d6
Oksana Polaikova <it.oksana.poliakova@gmail.com>
2022-10-19 17:47:15 +0300

    Initial Commit

---
+++ ToDoListSwiftUI/ToDoListSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListSwiftUIApp.swift
+//  ToDoListSwiftUI
+//
+//  Created by Oksana on 19.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
